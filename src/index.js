610d34d737a4227a27d102f85df561f019d19700
Pritam Kumar <pritam.kumar01@nagarro.com>
2023-04-16 18:40:43 +0530

    Final stuff added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./componenets/App";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+import "./styles/index.scss";
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
